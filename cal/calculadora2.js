6003d0eee6b6e3254010036310ec3ddc3e0ab202
Esmeralda-uwu <114690086+Esmeralda-uwu@users.noreply.github.com>
2023-05-19 12:03:17 -0600

    4456

---
+++ cal/calculadora2.js
@@ -0,0 +1,12 @@
+//var auxcall = 1
+
+//function agregarnumero(numero){
+  //  var resultado = document.getElementById("resultado");
+    //resultado.value += numero;
+//}
+/*function agregaroperador(operador){
+    var resultado = document.getElementById("resultado");
+    resultado.value += resultado;
+}*/
+
+
